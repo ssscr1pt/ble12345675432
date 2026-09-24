deb0c4038933854b3f31bca56b81abe38c48bec9
ssscr1pt <66695637+ssscr1pt@users.noreply.github.com>
2026-09-24 09:48:11 +0300

    Add BleSpamProtocol array and count definition

--- fl-BLE_SPAM-main/protocols/continuity.c
+++ fl-BLE_SPAM-main/protocols/continuity.c
@@ -1,3 +1,12 @@
+#include "_registry.h"
+
+const BleSpamProtocol* ble_spam_protocols[] = {
+    &protocol_continuity,
+    &protocol_fastpair,
+    &protocol_swiftpair,
+};
+
+const size_t ble_spam_protocols_count = COUNT_OF(ble_spam_protocols);
 #include "continuity.h"
 #include <storage/storage.h>
 
